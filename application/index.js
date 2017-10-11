12aa210c28b8a5ab1457def1c68c5da3d457a486
Santhos Baala RS <rssanthosbaala@gmail.com>
2017-10-11 14:58:35 +0200

    checkpoint - streaming

--- application/index.js
+++ application/index.js
@@ -1,10 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
+var path = require("path");
+var fs = require("fs");
+var childProcess = require("child_process");
 var PORT = 3000;
 var app = express();
-app.get('/', function (req, res) {
-    res.status(200).end('Hello');
+app.get('/process', function (req, res) {
+    var inputPath = path.resolve(__dirname, 'konrad.JPG');
+    var readStream = fs.createReadStream(inputPath);
+    var proc = childProcess.spawn('magick', [
+        '-',
+        '-scale',
+        '25%',
+        '-rotate',
+        '90',
+        '-'
+    ]);
+    proc.stdout.pipe(res);
+    readStream.pipe(proc.stdin);
 });
 app.listen(PORT, function () {
     console.log('Listening ' + PORT);
--- application/index.ts
+++ application/index.ts
@@ -1,11 +1,28 @@
 import * as express from 'express'
 
+import * as path from 'path'
+import * as fs from 'fs'
+import * as childProcess from 'child_process'
+
 const PORT = 3000
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.status(200).end('Hello')
+app.get('/process', (req, res) => {
+    const inputPath = path.resolve(__dirname, 'konrad.JPG')
+    const readStream = fs.createReadStream(inputPath)
+
+    const proc = childProcess.spawn('magick', [
+        '-', 
+        '-scale', 
+        '25%', 
+        '-rotate',
+        '90',
+        '-'
+    ])
+
+    proc.stdout.pipe(res)
+    readStream.pipe(proc.stdin)
 })
 
 app.listen(PORT, () => {
