0ae1333b27650c109eed7a66305bba6fc96ce6b5
Konrad Kleeberger <git@konrad.online>
2017-10-28 23:00:03 +0200

    add randomTasks

--- workload/index.ts
+++ workload/index.ts
@@ -1,12 +1,13 @@
-function mathFun(x) {
-  return - Math.pow((x - 2 * Math.sqrt(5)), 2) + 20
-}
-
 import * as fs from 'fs'
 import * as request from 'request-promise-native'
 
 const urls = fs.readFileSync('urls.txt').toString().split("\n");
-const operations = ['scale', 'rotation', 'resize']
+
+function mathFun(x: number) {
+  return - Math.pow((x - 2 * Math.sqrt(5)), 2) + 20
+}
+
+const masterUrl = "http://localhost:8000/add"
 
 const start = 0
 const end = 4 * Math.sqrt(5)
@@ -26,10 +27,40 @@ function* nextStep() {
 function randomLandsat() {
   const urlIdx = Math.floor(Math.random() * urls.length)
   const url = urls[urlIdx]
+  const tasks = randomTasks();
   
   return {
     source: url,
-    tasks: [["rotate", [-25]], ["scale", [25]]]
+    tasks
+  }
+}
+
+function randomTasks() {
+  return Array(Math.floor(Math.random() * 4 + 1)).fill(0).map(() => {
+    return randomTask()
+  })
+}
+
+function randomTask() {
+  const tasks = []
+  const operations = ['scale', 'rotation', 'resize']  
+  const operation = Math.floor(Math.random() * operations.length)
+  
+  switch (operation) {
+    case 0:
+      return ["scale", [Math.floor(Math.random() * 99 + 1)]]
+    case 1: 
+      return [["rotation"], [Math.floor(Math.random() * 359 + 1)]]
+    case 2: 
+      return [
+        ["resize"],
+          [
+            Math.floor(Math.random() * 100 + 1),
+            Math.floor(Math.random() * 100 + 1)
+          ]
+      ]
+    default:
+      break;
   }
 }
 
@@ -46,7 +77,7 @@ const timer = setInterval(() => {
   Promise.all(Array(amount).fill(0).map(() => {
     return request({
       method: 'POST',
-      uri: 'http://example.com/',
+      uri: masterUrl,
       body: randomLandsat(),
       json: true
     })
