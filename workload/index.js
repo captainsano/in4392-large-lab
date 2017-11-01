5dc485e8b87924d8fc18e5183ea6b8d67b65393c
Santhos Baala RS <rssanthosbaala@gmail.com>
2017-11-01 22:11:45 +0100

    Redefine workload using poisson process

--- workload/index.js
+++ workload/index.js
@@ -2,36 +2,29 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const path = require("path");
 const fs = require("fs");
-const axios_1 = require("axios");
+const PoissonProcess = require("poisson-process");
 const moment = require("moment");
-const constant_1 = require("./constant");
-const exponential_1 = require("./exponential");
-const spike_1 = require("./spike");
-const FUNCTION = (process.env.FUNCTION || '').toLowerCase();
 const HOST = process.env.HOST || 'localhost';
-if (['constant', 'exponential', 'spike'].indexOf(FUNCTION) < 0) {
-    console.log('Function not supported or unspecified');
+const INTERVAL = parseInt(process.env.INTERVAL || '0', 10);
+if (INTERVAL === 0) {
+    console.log('Specify interval');
     process.exit(1);
 }
-const BASE_DELAY = parseInt(process.env.BASE_DELAY || '100', 10);
-const TOTAL_REQUESTS = parseInt(process.env.TOTAL_REQUESTS || '1000', 10);
+const TOTAL_REQUESTS = parseInt(process.env.TOTAL_REQUESTS || '100', 10);
 const urls = fs.readFileSync(path.join(__dirname, '45-55-urls.txt')).toString().split('\n');
 const START_TIMESTAMP = moment().valueOf();
-let chosenFunction = null;
-if (FUNCTION === 'constant')
-    chosenFunction = constant_1.default;
-if (FUNCTION === 'exponential')
-    chosenFunction = exponential_1.default;
-if (FUNCTION === 'spike')
-    chosenFunction = spike_1.default;
-if (chosenFunction) {
-    chosenFunction(urls, BASE_DELAY, TOTAL_REQUESTS, (url, tick) => {
-        axios_1.default.post(`http://${HOST}:8000/add`, {
-            source: url,
-            tasks: [['scale', [25]], ['rotate', [90]]]
-        }).then(() => { }).catch(() => console.log('Error making request at tick: ', tick + 1));
-    })
-        .subscribe((i) => {
-        console.log(`${i + 1}\t${moment().valueOf() - START_TIMESTAMP}`);
-    });
-}
+let i = 0;
+const p = PoissonProcess.create(INTERVAL, () => {
+    console.log(`${i + 1}\t${moment().valueOf() - START_TIMESTAMP}`);
+    // axios.post(`http://${HOST}:8000/add`, {
+    //     source: urls[i],
+    //     tasks: [['scale', [25]], ['rotate', [90]]]
+    // }).then(() => {}).catch(() => console.log('Error making request at tick: ', i + 1))
+    if (i + 1 === TOTAL_REQUESTS) {
+        p.stop();
+    }
+    else {
+        i = i + 1;
+    }
+});
+p.start();
--- workload/index.ts
+++ workload/index.ts
@@ -1,43 +1,36 @@
 import * as path from 'path'
 import * as fs from 'fs'
-import {Observable} from 'rxjs'
 import axios from 'axios'
+import * as PoissonProcess from 'poisson-process'
 import * as moment from 'moment'
-import {Functions, TimingFunction} from './types'
 
-import constant from './constant'
-import exponential from './exponential'
-import spike from './spike'
-
-const FUNCTION = (process.env.FUNCTION || '').toLowerCase()
 const HOST = process.env.HOST || 'localhost'
+const INTERVAL = parseInt(process.env.INTERVAL || '0', 10)
 
-if (['constant', 'exponential', 'spike'].indexOf(FUNCTION) < 0) {
-    console.log('Function not supported or unspecified')
+if (INTERVAL === 0) {
+    console.log('Specify interval')
     process.exit(1)
 }
 
-const BASE_DELAY = parseInt(process.env.BASE_DELAY || '100', 10)
-const TOTAL_REQUESTS = parseInt(process.env.TOTAL_REQUESTS || '1000', 10)
+const TOTAL_REQUESTS = parseInt(process.env.TOTAL_REQUESTS || '100', 10)
 
 const urls = fs.readFileSync(path.join(__dirname, '45-55-urls.txt')).toString().split('\n')
 
 const START_TIMESTAMP = moment().valueOf()
 
-let chosenFunction = null
+let i = 0
+const p = PoissonProcess.create(INTERVAL, () => {
+    console.log(`${i + 1}\t${moment().valueOf() - START_TIMESTAMP}`)
+    axios.post(`http://${HOST}:8000/add`, {
+        source: urls[i],
+        tasks: [['scale', [25]], ['rotate', [90]]]
+    }).then(() => {}).catch(() => console.log('Error making request at tick: ', i + 1))
 
-if (FUNCTION === 'constant') chosenFunction = constant
-if (FUNCTION === 'exponential') chosenFunction = exponential
-if (FUNCTION === 'spike') chosenFunction = spike
+    if (i + 1 === TOTAL_REQUESTS) {
+        p.stop()
+    } else {
+        i = i + 1
+    }
+})
 
-if (chosenFunction) {
-    chosenFunction(urls, BASE_DELAY, TOTAL_REQUESTS, (url: string, tick: number) => {
-        axios.post(`http://${HOST}:8000/add`, {
-            source: url,
-            tasks: [['scale', [25]], ['rotate', [90]]]
-        }).then(() => {}).catch(() => console.log('Error making request at tick: ', tick + 1))
-    }) 
-    .subscribe((i) => {
-        console.log(`${i + 1}\t${moment().valueOf() - START_TIMESTAMP}`)
-    })
-}
+p.start()
