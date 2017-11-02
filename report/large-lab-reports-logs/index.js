6fe4cda72671df9a1d1dcca4f8157e2967b2d513
Konrad Kleeberger <git@konrad.online>
2017-11-02 22:58:01 +0100

    calculations for utilitization, instance and waiting time

--- report/large-lab-reports-logs/index.js
+++ report/large-lab-reports-logs/index.js
@@ -34,18 +34,30 @@ const getProcessingTime = function (report) {
 // Responsiveness (measure waiting time)
 // Execution - arrival time
 const getWaitingTime = function (report) {
-
+    return report.finishedTasks
+    .map((task) => {
+        return task.executeStartTime - task.arrivalTime
+    })
+    .reduce((a, b) => {
+        return a + b
+    }, 0)
 }
 
-// Instance termination - startTime
+// Instance ter.mination - startTime
 // Caution, termination might be null
 const getTotalInstanceTime = function (report) {
-
+    return report.terminatedInstances
+    .map((instance) => {
+        return moment.utc(instance.terminatedTime).valueOf() - moment.utc(instance.startTime).valueOf()
+    })
+    .reduce((a, b) => {
+        return a + b
+    }, 0)
 }
 
 // Total instance time - processing time
 const utilizationTime = function (report) {
-
+    return getProcessingTime(formatReport(report)) / getTotalInstanceTime(formatReport(report))
 }
 
 const formatReport = function(report) {
@@ -85,3 +97,29 @@ console.log()
 console.log('SE7-E2.5: ', getWaitingTime(formatReport(se7_e25)) / 1000, ' seconds')
 console.log('SE7-E5.0: ', getWaitingTime(formatReport(se7_e5)) / 1000, ' seconds')
 console.log('SE7-E7.5: ', getWaitingTime(formatReport(se7_e75)) / 1000, ' seconds')
+
+console.log('------- TotalInstanceTime --------')
+console.log('C3-E2.5: ', getTotalInstanceTime(formatReport(c3_e25)) / 1000, ' seconds')
+console.log('C3-E5.0: ', getTotalInstanceTime(formatReport(c3_e5)) / 1000, ' seconds')
+console.log('C3-E7.5: ', getTotalInstanceTime(formatReport(c3_e75)) / 1000, ' seconds')
+console.log()
+console.log('SE0-E2.5: ', getTotalInstanceTime(formatReport(se0_e25)) / 1000, ' seconds')
+console.log('SE0-E5.0: ', getTotalInstanceTime(formatReport(se0_e5)) / 1000, ' seconds')
+console.log('SE0-E7.5: ', getTotalInstanceTime(formatReport(se0_e75)) / 1000, ' seconds')
+console.log()
+console.log('SE7-E2.5: ', getTotalInstanceTime(formatReport(se7_e25)) / 1000, ' seconds')
+console.log('SE7-E5.0: ', getTotalInstanceTime(formatReport(se7_e5)) / 1000, ' seconds')
+console.log('SE7-E7.5: ', getTotalInstanceTime(formatReport(se7_e75)) / 1000, ' seconds')
+
+console.log('------- Utilization --------')
+console.log('C3-E2.5: ', utilizationTime(formatReport(c3_e25)))
+console.log('C3-E5.0: ', utilizationTime(formatReport(c3_e5)))
+console.log('C3-E7.5: ', utilizationTime(formatReport(c3_e75)))
+console.log()
+console.log('SE0-E2.5: ', utilizationTime(formatReport(se0_e25)))
+console.log('SE0-E5.0: ', utilizationTime(formatReport(se0_e5)))
+console.log('SE0-E7.5: ', utilizationTime(formatReport(se0_e75)))
+console.log()
+console.log('SE7-E2.5: ', utilizationTime(formatReport(se7_e25)))
+console.log('SE7-E5.0: ', utilizationTime(formatReport(se7_e5)))
+console.log('SE7-E7.5: ', utilizationTime(formatReport(se7_e75)))
