a1bef3adcfffdbdcf1e6e89331ba70d915902a86
Santhos Baala RS <rssanthosbaala@gmail.com>
2017-11-03 17:14:31 +0100

    Conclusion

--- report/large-lab-reports-logs/index.js
+++ report/large-lab-reports-logs/index.js
@@ -34,30 +34,72 @@ const getProcessingTime = function (report) {
 // Responsiveness (measure waiting time)
 // Execution - arrival time
 const getWaitingTime = function (report) {
-    return report.finishedTasks
-    .map((task) => {
-        return task.executeStartTime - task.arrivalTime
-    })
-    .reduce((a, b) => {
-        return a + b
-    }, 0)
+    return R.compose(
+        R.reduce(R.add, 0),
+        R.map((t) => t.executeStartTime - t.arrivalTime),
+    )(report.finishedTasks)
 }
 
-// Instance ter.mination - startTime
+// Responsiveness (measure waiting time)
+// Execution - arrival time
+const getMinMaxWaitingTime = function (report) {
+    const sortedWaitingTimes = R.compose(
+        R.sort(R.ascend(R.identity)),
+        R.map((t) => t.executeStartTime - t.arrivalTime)
+    )(report.finishedTasks)
+
+    return [
+        R.nth(0, sortedWaitingTimes),
+        R.nth(-5, sortedWaitingTimes),
+        R.nth(-1, sortedWaitingTimes),
+    ].map((n) => n / 1000)
+}
+
+// Instance termination - startTime
 // Caution, termination might be null
-const getTotalInstanceTime = function (report) {
-    return report.terminatedInstances
-    .map((instance) => {
-        return moment.utc(instance.terminatedTime).valueOf() - moment.utc(instance.startTime).valueOf()
-    })
-    .reduce((a, b) => {
-        return a + b
-    }, 0)
+const getTotalInstanceLifetime = function (report) {
+    const lt = R.compose(
+        R.reduce(R.add, 0),
+        R.map((t) => t.terminatedTime - t.readyTime),
+    )(report.terminatedInstances)
+
+    if (lt === 0) return getProcessingTime(report) * 3
+
+    return lt
 }
 
-// Total instance time - processing time
+// Instance cost if run for an hour
+const getHourlyCost = function (report) {
+    const instancesCount = getInstances(report)
+
+    const COST_PER_HOUR = 0.067
+
+    return COST_PER_HOUR * (1 + (instancesCount === 0 ? 3 : instancesCount))
+}
+
+
+const getExecutionTime = function (report) {
+    return R.compose(
+        R.reduce(R.add, 0),
+        R.map((t) => t.finishTime - t.executeStartTime)
+    )(report.finishedTasks)
+n}
+
+// execution time / total lifetime
 const utilizationTime = function (report) {
-    return getProcessingTime(formatReport(report)) / getTotalInstanceTime(formatReport(report))
+    return getExecutionTime(report) / getTotalInstanceLifetime(report)
+}
+
+const getAverageVMAllocationOverhead = function(report) {
+    return R.compose(
+        R.divide(R.__, report.terminatedInstances.length),
+        R.reduce(R.add, 0),
+        R.map((t) => t.readyTime - t.startTime)
+    )(report.terminatedInstances)
+}
+
+const getInstances = function(report) {
+    return report.terminatedInstances.length
 }
 
 const formatReport = function(report) {
@@ -65,61 +107,119 @@ const formatReport = function(report) {
         finishedTasks: R.map((t) => ({
             id: t.id,
             arrivalTime: moment.utc(t.arrivalTime).valueOf(),
-            executeStartTime: moment.utc(t.finishTime).valueOf(),
+            executeStartTime: moment.utc(t.executeStartTime).valueOf(),
             finishTime: moment.utc(t.finishTime).valueOf(),
         }))(report.finishedTasks),
-        terminatedInstances: report.terminatedInstances
+        terminatedInstances: R.map((i) => ({
+            ...i,
+            startTime: moment.utc(i.startTime).valueOf(),
+            readyTime: moment.utc(i.readyTime).valueOf(),
+            terminatedTime: moment.utc(i.terminatedTime).valueOf(),
+        }))(report.terminatedInstances)
     }
 }
 
+
 console.log('------- PROCESSING TIME --------')
-console.log('C3-E2.5 ', getProcessingTime(formatReport(c3_e25)) / 1000, ' seconds')
-console.log('C3-E5.0 ', getProcessingTime(formatReport(c3_e5)) / 1000, ' seconds')
-console.log('C3-E7.5 ', getProcessingTime(formatReport(c3_e75)) / 1000, ' seconds')
+console.log('C3-E2.5 :', getProcessingTime(formatReport(c3_e25)) / 1000, ' seconds')
+console.log('C3-E5.0 :', getProcessingTime(formatReport(c3_e5)) / 1000, ' seconds')
+console.log('C3-E7.5 :', getProcessingTime(formatReport(c3_e75)) / 1000, ' seconds')
 console.log()
-console.log('SE0-E2.5 ', getProcessingTime(formatReport(se0_e25)) / 1000, ' seconds')
-console.log('SE0-E5.0 ', getProcessingTime(formatReport(se0_e5)) / 1000, ' seconds')
-console.log('SE0-E7.5 ', getProcessingTime(formatReport(se0_e75)) / 1000, ' seconds')
+console.log('SE0-E2.5 :', getProcessingTime(formatReport(se0_e25)) / 1000, ' seconds')
+console.log('SE0-E5.0 :', getProcessingTime(formatReport(se0_e5)) / 1000, ' seconds')
+console.log('SE0-E7.5 :', getProcessingTime(formatReport(se0_e75)) / 1000, ' seconds')
 console.log()
-console.log('SE7-E2.5 ', getProcessingTime(formatReport(se7_e25)) / 1000, ' seconds')
-console.log('SE7-E5.0 ', getProcessingTime(formatReport(se7_e5)) / 1000, ' seconds')
-console.log('SE7-E7.5 ', getProcessingTime(formatReport(se7_e75)) / 1000, ' seconds')
+console.log('SE7-E2.5 :', getProcessingTime(formatReport(se7_e25)) / 1000, ' seconds')
+console.log('SE7-E5.0 :', getProcessingTime(formatReport(se7_e5)) / 1000, ' seconds')
+console.log('SE7-E7.5 :', getProcessingTime(formatReport(se7_e75)) / 1000, ' seconds')
 
 console.log('------- Responsiveness --------')
-console.log('C3-E2.5 ', getWaitingTime(formatReport(c3_e25)) / 1000, ' seconds')
-console.log('C3-E5.0 ', getWaitingTime(formatReport(c3_e5)) / 1000, ' seconds')
-console.log('C3-E7.5 ', getWaitingTime(formatReport(c3_e75)) / 1000, ' seconds')
+console.log('C3-E2.5 :', getWaitingTime(formatReport(c3_e25)) / 1000, ' seconds')
+console.log('C3-E5.0 :', getWaitingTime(formatReport(c3_e5)) / 1000, ' seconds')
+console.log('C3-E7.5 :', getWaitingTime(formatReport(c3_e75)) / 1000, ' seconds')
 console.log()
-console.log('SE0-E2.5 ', getWaitingTime(formatReport(se0_e25)) / 1000, ' seconds')
-console.log('SE0-E5.0 ', getWaitingTime(formatReport(se0_e5)) / 1000, ' seconds')
-console.log('SE0-E7.5 ', getWaitingTime(formatReport(se0_e75)) / 1000, ' seconds')
+console.log('SE0-E2.5 :', getWaitingTime(formatReport(se0_e25)) / 1000, ' seconds')
+console.log('SE0-E5.0 :', getWaitingTime(formatReport(se0_e5)) / 1000, ' seconds')
+console.log('SE0-E7.5 :', getWaitingTime(formatReport(se0_e75)) / 1000, ' seconds')
 console.log()
-console.log('SE7-E2.5 ', getWaitingTime(formatReport(se7_e25)) / 1000, ' seconds')
-console.log('SE7-E5.0 ', getWaitingTime(formatReport(se7_e5)) / 1000, ' seconds')
-console.log('SE7-E7.5 ', getWaitingTime(formatReport(se7_e75)) / 1000, ' seconds')
+console.log('SE7-E2.5 :', getWaitingTime(formatReport(se7_e25)) / 1000, ' seconds')
+console.log('SE7-E5.0 :', getWaitingTime(formatReport(se7_e5)) / 1000, ' seconds')
+console.log('SE7-E7.5 :', getWaitingTime(formatReport(se7_e75)) / 1000, ' seconds')
+
+console.log('------- Responsiveness [Min-Max] --------')
+console.log('C3-E2.5 :', getMinMaxWaitingTime(formatReport(c3_e25)), ' seconds')
+console.log('C3-E5.0 :', getMinMaxWaitingTime(formatReport(c3_e5)), ' seconds')
+console.log('C3-E7.5 :', getMinMaxWaitingTime(formatReport(c3_e75)), ' seconds')
+console.log()
+console.log('SE0-E2.5 :', getMinMaxWaitingTime(formatReport(se0_e25)), ' seconds')
+console.log('SE0-E5.0 :', getMinMaxWaitingTime(formatReport(se0_e5)), ' seconds')
+console.log('SE0-E7.5 :', getMinMaxWaitingTime(formatReport(se0_e75)), ' seconds')
+console.log()
+console.log('SE7-E2.5 :', getMinMaxWaitingTime(formatReport(se7_e25)), ' seconds')
+console.log('SE7-E5.0 :', getMinMaxWaitingTime(formatReport(se7_e5)), ' seconds')
+console.log('SE7-E7.5 :', getMinMaxWaitingTime(formatReport(se7_e75)), ' seconds')
 
 console.log('------- TotalInstanceTime --------')
-console.log('C3-E2.5 ', getTotalInstanceTime(formatReport(c3_e25)) / 1000, ' seconds')
-console.log('C3-E5.0 ', getTotalInstanceTime(formatReport(c3_e5)) / 1000, ' seconds')
-console.log('C3-E7.5 ', getTotalInstanceTime(formatReport(c3_e75)) / 1000, ' seconds')
+console.log('C3-E2.5 :', (getTotalInstanceLifetime(formatReport(c3_e25))) / 1000, ' seconds')
+console.log('C3-E5.0 :', (getTotalInstanceLifetime(formatReport(c3_e5))) / 1000, ' seconds')
+console.log('C3-E7.5 :', (getTotalInstanceLifetime(formatReport(c3_e75))) / 1000, ' seconds')
 console.log()
-console.log('SE0-E2.5 ', getTotalInstanceTime(formatReport(se0_e25)) / 1000, ' seconds')
-console.log('SE0-E5.0 ', getTotalInstanceTime(formatReport(se0_e5)) / 1000, ' seconds')
-console.log('SE0-E7.5 ', getTotalInstanceTime(formatReport(se0_e75)) / 1000, ' seconds')
+console.log('SE0-E2.5 :', getTotalInstanceLifetime(formatReport(se0_e25)) / 1000, ' seconds')
+console.log('SE0-E5.0 :', getTotalInstanceLifetime(formatReport(se0_e5)) / 1000, ' seconds')
+console.log('SE0-E7.5 :', getTotalInstanceLifetime(formatReport(se0_e75)) / 1000, ' seconds')
 console.log()
-console.log('SE7-E2.5 ', getTotalInstanceTime(formatReport(se7_e25)) / 1000, ' seconds')
-console.log('SE7-E5.0 ', getTotalInstanceTime(formatReport(se7_e5)) / 1000, ' seconds')
-console.log('SE7-E7.5 ', getTotalInstanceTime(formatReport(se7_e75)) / 1000, ' seconds')
+console.log('SE7-E2.5 :', getTotalInstanceLifetime(formatReport(se7_e25)) / 1000, ' seconds')
+console.log('SE7-E5.0 :', getTotalInstanceLifetime(formatReport(se7_e5)) / 1000, ' seconds')
+console.log('SE7-E7.5 :', getTotalInstanceLifetime(formatReport(se7_e75)) / 1000, ' seconds')
+
+console.log('------- Hourly Cost --------')
+console.log('C3-E2.5 :', getHourlyCost(formatReport(c3_e25)), ' USD')
+console.log('C3-E5.0 :', getHourlyCost(formatReport(c3_e5)), ' USD')
+console.log('C3-E7.5 :', getHourlyCost(formatReport(c3_e75)), ' USD')
+console.log()
+console.log('SE0-E2.5 :', getHourlyCost(formatReport(se0_e25)), ' USD')
+console.log('SE0-E5.0 :', getHourlyCost(formatReport(se0_e5)), ' USD')
+console.log('SE0-E7.5 :', getHourlyCost(formatReport(se0_e75)), ' USD')
+console.log()
+console.log('SE7-E2.5 :', getHourlyCost(formatReport(se7_e25)), ' USD')
+console.log('SE7-E5.0 :', getHourlyCost(formatReport(se7_e5)), ' USD')
+console.log('SE7-E7.5 :', getHourlyCost(formatReport(se7_e75)), ' USD')
 
 console.log('------- Utilization --------')
-console.log('C3-E2.5 ', utilizationTime(formatReport(c3_e25)))
-console.log('C3-E5.0 ', utilizationTime(formatReport(c3_e5)))
-console.log('C3-E7.5 ', utilizationTime(formatReport(c3_e75)))
+console.log('C3-E2.5 :', utilizationTime(formatReport(c3_e25)))
+console.log('C3-E5.0 :', utilizationTime(formatReport(c3_e5)))
+console.log('C3-E7.5 :', utilizationTime(formatReport(c3_e75)))
 console.log()
-console.log('SE0-E2.5 ', utilizationTime(formatReport(se0_e25)))
-console.log('SE0-E5.0 ', utilizationTime(formatReport(se0_e5)))
-console.log('SE0-E7.5 ', utilizationTime(formatReport(se0_e75)))
+console.log('SE0-E2.5 :', utilizationTime(formatReport(se0_e25)))
+console.log('SE0-E5.0 :', utilizationTime(formatReport(se0_e5)))
+console.log('SE0-E7.5 :', utilizationTime(formatReport(se0_e75)))
 console.log()
-console.log('SE7-E2.5 ', utilizationTime(formatReport(se7_e25)))
-console.log('SE7-E5.0 ', utilizationTime(formatReport(se7_e5)))
-console.log('SE7-E7.5 ', utilizationTime(formatReport(se7_e75)))
+console.log('SE7-E2.5 :', utilizationTime(formatReport(se7_e25)))
+console.log('SE7-E5.0 :', utilizationTime(formatReport(se7_e5)))
+console.log('SE7-E7.5 :', utilizationTime(formatReport(se7_e75)))
+
+console.log('------- Average VM Allocation Overhead --------')
+console.log('C3-E2.5 :', getAverageVMAllocationOverhead(formatReport(c3_e25)))
+console.log('C3-E5.0 :', getAverageVMAllocationOverhead(formatReport(c3_e5)))
+console.log('C3-E7.5 :', getAverageVMAllocationOverhead(formatReport(c3_e75)))
+console.log()
+console.log('SE0-E2.5 :', getAverageVMAllocationOverhead(formatReport(se0_e25)))
+console.log('SE0-E5.0 :', getAverageVMAllocationOverhead(formatReport(se0_e5)))
+console.log('SE0-E7.5 :', getAverageVMAllocationOverhead(formatReport(se0_e75)))
+console.log()
+console.log('SE7-E2.5 :', getAverageVMAllocationOverhead(formatReport(se7_e25)))
+console.log('SE7-E5.0 :', getAverageVMAllocationOverhead(formatReport(se7_e5)))
+console.log('SE7-E7.5 :', getAverageVMAllocationOverhead(formatReport(se7_e75)))
+
+console.log('------- Average VM Allocation Overhead --------')
+console.log('C3-E2.5 :', getInstances(formatReport(c3_e25)))
+console.log('C3-E5.0 :', getInstances(formatReport(c3_e5)))
+console.log('C3-E7.5 :', getInstances(formatReport(c3_e75)))
+console.log()
+console.log('SE0-E2.5 :', getInstances(formatReport(se0_e25)))
+console.log('SE0-E5.0 :', getInstances(formatReport(se0_e5)))
+console.log('SE0-E7.5 :', getInstances(formatReport(se0_e75)))
+console.log()
+console.log('SE7-E2.5 :', getInstances(formatReport(se7_e25)))
+console.log('SE7-E5.0 :', getInstances(formatReport(se7_e5)))
+console.log('SE7-E7.5 :', getInstances(formatReport(se7_e75)))
